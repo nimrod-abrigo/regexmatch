8dfd8de1b630a4d8ad03d7e22f5aebdf4da5da66
Nimrod Abrigo <abrigonimrod@gmail.com>
2016-09-15 08:20:09 +0800

    Adds another function for regex

--- regexmatch/src/regexmatch/Regexmatch.java
+++ regexmatch/src/regexmatch/Regexmatch.java
@@ -5,9 +5,14 @@ public class Regexmatch {
     public static boolean isTrue(String str) {
         return str.matches("[Tt]rue");
     }
+    
+    public static boolean containsFalse(String str){
+        return str.matches(".*false.*");
+    }
 
     public static void main(String[] args) {
         System.out.println(isTrue("true"));
+        System.out.println(containsFalse("lkgfalsesh"));
     }
 
 }
